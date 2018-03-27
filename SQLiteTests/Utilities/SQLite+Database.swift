6a7ca3a9f53fa3bd71838db5fd595e395ea5de8a
Dima Bart <dima.bart01@gmail.com>
2018-03-27 19:39:18 -0400

    Seed the test database in a transaction.

--- SQLiteTests/Utilities/SQLite+Database.swift
+++ SQLiteTests/Utilities/SQLite+Database.swift
@@ -28,18 +28,22 @@ extension SQLite {
         let result = try! sqlite.execute(query: query)
         XCTAssertEqual(result, .done)
         
-        try! sqlite.execute(query: "INSERT INTO animal VALUES (1, 'bulldog', 'dog', 44.76, NULL, NULL);")
-        try! sqlite.execute(query: "INSERT INTO animal VALUES (2, 'red squirrel', 'rodent', 23.34, NULL, NULL);")
-        try! sqlite.execute(query: "INSERT INTO animal VALUES (3, NULL, 'mammal', 4279.281, X'FEEDBEEF', NULL);")
-        try! sqlite.execute(query: "INSERT INTO animal VALUES (4, 'tiger', 'feline', 1321.84, NULL, NULL);")
-        try! sqlite.execute(query: "INSERT INTO animal VALUES (5, 'lion', 'feline', 1185.48, NULL, NULL);")
-        try! sqlite.execute(query: "INSERT INTO animal VALUES (6, 'puma', 'feline', 978.52, NULL, NULL);")
-        try! sqlite.execute(query: "INSERT INTO animal VALUES (7, 'black squirrel', 'rodent', 37.82, NULL, NULL);")
-        try! sqlite.execute(query: "INSERT INTO animal VALUES (8, 'greyhound', 'dog', 63.578, NULL, NULL);")
-        try! sqlite.execute(query: "INSERT INTO animal VALUES (9, 'aligator', 'reptile', 3892.75, NULL, NULL);")
-        try! sqlite.execute(query: "INSERT INTO animal VALUES (10, 'crocodile', 'reptile', 2987.83, NULL, NULL);")
-        try! sqlite.execute(query: "INSERT INTO animal VALUES (11, 'iguana', 'reptile', 39.92, NULL, NULL);")
-        try! sqlite.execute(query: "INSERT INTO animal VALUES (999, 'dragon', 'mythical', NULL, NULL, NULL);")
+        try! sqlite.performTransaction(.deferred) {
+            try sqlite.execute(query: "INSERT INTO animal VALUES (1, 'bulldog', 'dog', 44.76, NULL, NULL);")
+            try sqlite.execute(query: "INSERT INTO animal VALUES (2, 'red squirrel', 'rodent', 23.34, NULL, NULL);")
+            try sqlite.execute(query: "INSERT INTO animal VALUES (3, NULL, 'mammal', 4279.281, X'FEEDBEEF', NULL);")
+            try sqlite.execute(query: "INSERT INTO animal VALUES (4, 'tiger', 'feline', 1321.84, NULL, NULL);")
+            try sqlite.execute(query: "INSERT INTO animal VALUES (5, 'lion', 'feline', 1185.48, NULL, NULL);")
+            try sqlite.execute(query: "INSERT INTO animal VALUES (6, 'puma', 'feline', 978.52, NULL, NULL);")
+            try sqlite.execute(query: "INSERT INTO animal VALUES (7, 'black squirrel', 'rodent', 37.82, NULL, NULL);")
+            try sqlite.execute(query: "INSERT INTO animal VALUES (8, 'greyhound', 'dog', 63.578, NULL, NULL);")
+            try sqlite.execute(query: "INSERT INTO animal VALUES (9, 'aligator', 'reptile', 3892.75, NULL, NULL);")
+            try sqlite.execute(query: "INSERT INTO animal VALUES (10, 'crocodile', 'reptile', 2987.83, NULL, NULL);")
+            try sqlite.execute(query: "INSERT INTO animal VALUES (11, 'iguana', 'reptile', 39.92, NULL, NULL);")
+            try sqlite.execute(query: "INSERT INTO animal VALUES (999, 'dragon', 'mythical', NULL, NULL, NULL);")
+            
+            return .commit
+        }
         
         sqlite.lastInsertID = 0
         
